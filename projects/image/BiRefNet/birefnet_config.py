898ead00ea42dac6b097c6eb620e08115eb889bc
blueshakeLam <blueshake2005@outlook.com>
2025-10-03 14:14:36 +0800

    Create birefnet_config.py

---
+++ projects/image/BiRefNet/birefnet_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import os
+
+
+def get_dir_path():
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+def get_model_path():
+    return get_dir_path() + "/model"
+
+
+def get_birefnet_model_path():
+    return get_model_path() + "/BiRefNet"
